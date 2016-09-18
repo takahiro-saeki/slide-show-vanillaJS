1973751aa1cd42fad2b6f8f1e27e58ee93a66e19
takahiro-saeki <hiroooon69@gmail.com>
2016-09-17 22:44:35 -0700

    delete unnecessary code.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # slide-show-vanillaJS
 this repository is sample of slideshow with vanillaJS.
+
+##TASK
+- refactoring.
+- make another animate options.
--- assets/js/common.js
+++ assets/js/common.js
@@ -4,10 +4,12 @@ class Test {
   constructor(options) {
     this.options = Object.assign({
       elem: '#app',
-      time: 1000
+      time: 1000,
+      animate: 'normal'
     }, options)
     this.elem = this.options['elem'];
     this.time = this.options['time'];
+    this.animate = this.options['animate'];
     const count = this.count;
     this.autoChange();
     this.moveRight();
@@ -24,13 +26,13 @@ class Test {
   }
 
   slide(box, interval, length) {
-    box[this.count].classList.remove('show');
+    let animateClass = 'show';
+    box[this.count].classList.remove(animateClass);
     this.count++;
     if(this.count >= length) {
       this.count = 0;
     }
-    box[this.count].classList.add('show')
-    box[this.count].classList.remove('fadeOutLeft');
+    box[this.count].classList.add(animateClass)
   }
 
   moveRight() {
@@ -51,17 +53,15 @@ class Test {
     const box = document.querySelectorAll('.responsive-img');
     const left = document.querySelector('.left')
     left.addEventListener('click', e => {
-      box[this.count].classList.add('fadeOutLeft')
       box[this.count].classList.remove('show');
       this.count--;
       if(this.count < 0) {
         this.count = 3;
       }
       box[this.count].classList.add('show')
-      box[this.count].classList.remove('fadeOutLeft');
       console.log(this.count)
     })
   }
 }
 
-new Test({time: 3000});
+new Test({time: 3000, animate: 'animate'});
